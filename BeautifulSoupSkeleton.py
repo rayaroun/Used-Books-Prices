0413eebd6a3a4511b6eca116ac7c6773645f2a7a
Aroun A. Dalawat <ray.aroun@gmail.com>
2021-09-03 01:10:20 -0700

    basic get url

--- BeautifulSoupSkeleton.py
+++ BeautifulSoupSkeleton.py
@@ -1,2 +1,23 @@
 import csv
 from bs4 import BeautifulSoup   
+from selenium import webdriver
+
+
+#chrome driver
+
+driver = webdriver.Chrome()
+
+url = "https://www.amazon.ca/"
+
+# driver.get(url)
+
+def get_url(search_term : str):
+    #https://www.amazon.ca/s?k=978-0195626438&ref=nb_sb_noss
+
+    template = "https://www.amazon.ca/s?k={}&ref=nb_sb_noss"
+    search_term = search_term.replace(" " , "+")
+    return template.format(search_term)
+
+
+url = get_url('978-0195626438')
+driver.get(url)
