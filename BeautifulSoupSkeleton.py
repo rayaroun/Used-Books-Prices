80388e9b0768d9c484f935be7d4929bae59c2164
Aroun A. Dalawat <ray.aroun@gmail.com>
2021-09-06 23:43:07 -0700

    adding some fucntions

--- BeautifulSoupSkeleton.py
+++ BeautifulSoupSkeleton.py
@@ -1,5 +1,5 @@
-
 import csv
+import requests
 from bs4 import BeautifulSoup   
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,13 +14,7 @@ class scraper:
         self.driver = webdriver.Chrome(options=chrome_options)
         print(self.isbn)
 
-    #chrome driver
 
-    # driver = webdriver.Chrome()
-
-    # url = "https://www.amazon.ca/"
-
-    # driver.get(url)
 
     def get_url(self):
         #https://www.amazon.ca/s?k=978-0195626438&ref=nb_sb_noss
@@ -30,6 +24,34 @@ class scraper:
         self.url =  template.format(self.isbn)
         print(self.url)
         self.driver.get(self.url)
+        return self.url
+
+
+
+    def get_used_books_link(self):
+
+
+        self.soup = BeautifulSoup(self.driver.page_source,'html.parser')
+
+        self.results = self.soup.find_all('span', {'class' : 'a-declarative'})
+
+        print(self.results)
+
+
+    
+    def get_soup(self,url):
+        response = requests.get(url)
+        if response.ok:
+            return BeautifulSoup(response.text, features="html.parser")
+
+    def get_links(self,soup):
+        links = []
+        for tag in soup.findAll("a", href=True):
+            if img := tag.img:
+                img = img.get("src")
+            links.append(dict(url=tag.get("href"), text=tag.text, img=img))
+        return links
+
         
 
 
@@ -41,4 +63,6 @@ class scraper:
 
 
 khushwant = scraper("978-0195626438")
-khushwant.get_url()
+url = khushwant.get_url()
+khushwant.get_used_books_link()
+
